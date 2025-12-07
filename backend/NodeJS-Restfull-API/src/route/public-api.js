3542b19b5b46664036a786641f2dc56d5f255009
Fahrul187 <fahrulmuhammad764@gmail.com>
2025-12-07 19:50:52 +0700

    add feedback

--- backend/NodeJS-Restfull-API/src/route/public-api.js
+++ backend/NodeJS-Restfull-API/src/route/public-api.js
@@ -2,6 +2,13 @@ import express from 'express';
 import userController from '../controller/user-controller.js';
 
 const publicRouter = new express.Router();
+
+publicRouter.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Hello World! Backend is running."
+    });
+});
+
 publicRouter.post('/api/users', userController.register);
 publicRouter.post('/api/users/login', userController.login);
 
